72178088b8a6d96aa6793ec7a5255eb65dc2ec5c
Adrian Czerwiec <33912477+czerwiukk@users.noreply.github.com>
2024-09-17 09:01:11 +0200

    Initialize React tutorial, slightly change structure (#5)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial-react-native/installation"
+            to="/guide/react-native/installation"
           >
-            Setup Mobile App - 5min ⏱️
+            Setup React Native App - 5min ⏱️
           </Link>
         </div>
       </div>
