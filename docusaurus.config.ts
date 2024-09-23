bd251b4a9e23edbc246795c050ad06968c2e4d76
Miron Pawlik <mironiasty@users.noreply.github.com>
2024-09-23 10:31:18 +0200

    Update mobile documentation (#13)
    ## Description
    
    Add more info about mobile docs
    
    Add plugin for npm/yarn tab generation
    
    Enable `bash` syntax coloring
    
    Add new social card (two lines of text, instead of three

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,6 +37,9 @@ const config: Config = {
           path: "guide",
           routeBasePath: "guide",
           editUrl: "https://github.com/fishjam-cloud/documentation/",
+          remarkPlugins: [
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
+          ],
         },
         blog: {
           showReadingTime: true,
@@ -56,26 +59,7 @@ const config: Config = {
     ],
   ],
 
-  plugins: [
-    // [
-    //   "@docusaurus/plugin-content-docs",
-    //   {
-    //     id: "react",
-    //     path: "react-docs",
-    //     routeBasePath: "react",
-    //     sidebarPath: "./sidebars/react.ts",
-    //   },
-    // ],
-    // [
-    //   "@docusaurus/plugin-content-docs",
-    //   {
-    //     id: "react-native",
-    //     path: "react-native-docs",
-    //     routeBasePath: "react-native",
-    //     sidebarPath: "./sidebars/react-native.ts",
-    //   },
-    // ],
-  ],
+  plugins: [],
 
   themeConfig: {
     image: "img/social-card.png",
@@ -96,18 +80,7 @@ const config: Config = {
           position: "left",
           sidebarId: "guideSidebar",
         },
-        // {
-        //   to: "/react-native",
-        //   label: "React Native",
-        //   position: "left",
-        //   sidebarId: "reactNativeSidebar",
-        // },
-        // {
-        //   to: "/react",
-        //   label: "React",
-        //   position: "left",
-        //   sidebarId: "reactSidebar",
-        // },
+
         {
           href: "https://fishjam.io/app/",
           label: "Fishjam Dashboard",
@@ -159,6 +132,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.gruvboxMaterialLight,
       darkTheme: prismThemes.gruvboxMaterialDark,
+      additionalLanguages: ["bash"],
     },
   } satisfies Preset.ThemeConfig,
 };
