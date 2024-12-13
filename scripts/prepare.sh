86e744922e1330970e5c7b8ce056a9bcae31f236
Miron Pawlik <mironiasty@users.noreply.github.com>
2024-12-13 15:25:55 +0100

    [FCE-980] Add typedoc documentation  (#69)
    ## Description
    
    Integrate docusarus with typedoc.
    
    - This PR adds gitmodules with mobile and web repo inside documentation.
    - Adds `docusaurus-plugin-typedoc` plugin to generate typedoc
    
    Note that in order to get latest changes in client SDK code, you will
    have to update modules reference points.
    
    To test these changes locally, run:
    ```
    yarn
    yarn prepare
    yarn start
    ```

---
+++ scripts/prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+ROOTDIR=$(dirname $(dirname "$(readlink -f $0)"))
+echo $ROOTDIR
+cd $ROOTDIR
+
+printf "Synchronising submodules... "
+git submodule sync >> /dev/null
+git submodule update --init >> /dev/null
+
+cd packages/web-client-sdk/
+yarn && yarn build
+
+cd $ROOTDIR
+
+cd packages/mobile-client-sdk/
+yarn
+printf "DONE\n"
