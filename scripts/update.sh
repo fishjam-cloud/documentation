3e57bd14f24c9d13187a0ea216997330dd022589
Miron Pawlik <miron.pawlik@swmansion.com>
2025-02-11 12:14:48 +0100

    Update scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,23 +4,20 @@ set -e
 
 ROOTDIR=$(dirname $(dirname "$(readlink -f $0)"))
 echo $ROOTDIR
+
 cd $ROOTDIR
-
-printf "Synchronising submodules... "
-git submodule sync >> /dev/null
-git submodule update --init >> /dev/null
-
 cd packages/web-client-sdk/
-yarn && yarn build
+git checkout main
+git pull
 
 cd $ROOTDIR
-
 cd packages/mobile-client-sdk/
-yarn
+git checkout main
+git pull
 
 cd $ROOTDIR
-
 cd packages/js-server-sdk/
-yarn && yarn build
+git checkout main
+git pull
 
-printf "DONE\n"
+.
