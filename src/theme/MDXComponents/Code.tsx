f503adf0e8cabfcd2612fa28b7439bab152d0595
PiotrWodecki <Piotr.M.Wodecki@gmail.com>
2025-06-30 19:21:36 +0200

    Use yarn workspaces instead of imports

--- src/theme/MDXComponents/Code.tsx
+++ src/theme/MDXComponents/Code.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import CodeInline from "@theme/CodeInline";
 import type { Props } from "@theme/MDXComponents/Code";
 
-// imports for doc type checking purposes
-import "../../../packages/web-client-sdk/packages/react-client/src/types/public";
-import "../../../packages/mobile-client-sdk/packages/react-native-client/src/types";
-import "../../../packages/js-server-sdk/packages/js-server-sdk/src/types";
-
 function shouldBeInline(props: Props) {
   return (
     // empty code blocks have no props.children,
