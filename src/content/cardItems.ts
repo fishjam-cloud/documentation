33542cf5c7a453a925b4f123b17ba6fe53cf9f20
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-07-02 10:47:28 +0200

    restructrate main page

--- src/content/cardItems.ts
+++ src/content/cardItems.ts
@@ -1,8 +1,6 @@
 import { CardItem } from "@site/types";
 
 export const items: CardItem[] = [
-  { title: "Introduction", href: "/introduction", icon: "👋" },
-  { title: "Glossary", href: "/glossary", icon: "📜" },
   {
     title: "Quick setup with React Native",
     href: "/react-native/quick-setup",
