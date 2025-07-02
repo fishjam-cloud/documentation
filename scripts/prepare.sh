fbdce588fd7d3ae16de076384920c11f5e203660
PiotrWodecki <Piotr.M.Wodecki@gmail.com>
2025-07-02 15:56:00 +0200

    Revert CI hacks

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,41 +10,17 @@ printf "Synchronising submodules... "
 git submodule sync >> /dev/null
 git submodule update --init >> /dev/null
 
-
-cd $ROOTDIR
-cd packages/mobile-client-sdk/
-yarn && yarn build
-
-cd $ROOTDIR
-cd packages/js-server-sdk/
-yarn && yarn build
-
-cd $ROOTDIR
 cd packages/web-client-sdk/
+yarn && yarn build
+
+cd $ROOTDIR
+
+cd packages/mobile-client-sdk/
 yarn
 
 cd $ROOTDIR
-cd packages/web-client-sdk/packages/webrtc-client/
-yarn build
 
-cd $ROOTDIR
-cd packages/mobile-client-sdk/packages/react-native-client/
-yarn pack
-
-cd $ROOTDIR
-cd packages/js-server-sdk/packages/js-server-sdk/
-yarn pack
-
-cd $ROOTDIR
-cd packages/web-client-sdk/packages/react-client/
-yarn pack
-
-cd $ROOTDIR
-yarn install --immutable
-
-cd $ROOTDIR
-cd packages/web-client-sdk/packages/react-client/
-yarn build && yarn pack
-
+cd packages/js-server-sdk/
+yarn && yarn build
 
 printf "DONE\n"
