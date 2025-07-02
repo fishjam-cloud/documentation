f19d44e64470c3992cbd8ce035c33bc7ac6f22ea
PiotrWodecki <Piotr.M.Wodecki@gmail.com>
2025-07-02 13:31:38 +0200

    Run prepare early in checks

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,33 +10,34 @@ printf "Synchronising submodules... "
 git submodule sync >> /dev/null
 git submodule update --init >> /dev/null
 
+cd $ROOTDIR
 cd packages/web-client-sdk/
-yarn && yarn build
+yarn
 
 cd $ROOTDIR
-
 cd packages/web-client-sdk/packages/react-client/
 yarn pack
 
 cd $ROOTDIR
-
 cd packages/mobile-client-sdk/
-yarn && yarn build
+yarn
 
 cd $ROOTDIR
-
-
-cd packages/mobile-client-sdk/packages/react-native-client/
-yarn pack
-
-cd $ROOTDIR
-
 cd packages/js-server-sdk/
-yarn && yarn build
+yarn
+
 
 cd $ROOTDIR
+cd packages/mobile-client-sdk/packages/react-native-client/
+yarn build && yarn pack
 
+cd $ROOTDIR
+cd packages/web-client-sdk/packages/react-client/
+yarn build && yarn pack
+
+
+cd $ROOTDIR
 cd packages/js-server-sdk/packages/js-server-sdk/
-yarn pack
+yarn build && yarn pack
 
 printf "DONE\n"
