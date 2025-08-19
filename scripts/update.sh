66644b56e6d6f2d79278fa6f29d6f095d5c17cd9
Przemysław Rożnawski <48837433+roznawsk@users.noreply.github.com>
2025-08-19 15:45:34 +0200

    Fce 1833 document api (#158)
    Added API Reference page and a script to fetch the latest api
    documentation versions from relevant repositories.
    
    Resolves #135

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,17 +5,5 @@ set -e
 ROOTDIR=$(dirname $(dirname "$(readlink -f $0)"))
 echo $ROOTDIR
 
-cd $ROOTDIR
-cd packages/web-client-sdk/
-git checkout main
-git pull
-
-cd $ROOTDIR
-cd packages/mobile-client-sdk/
-git checkout main
-git pull
-
-cd $ROOTDIR
-cd packages/js-server-sdk/
-git checkout main
-git pull
+git submodule init
+git submodule update --remote
