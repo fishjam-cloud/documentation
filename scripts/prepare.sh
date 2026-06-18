23b6f3a623991691cd40c3e530dbda3c81b89006
Milosz Filimowski <milosz.filimowski@swmansion.com>
2026-06-18 14:15:57 +0200

    fix ci

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,6 +10,15 @@ printf "Synchronising submodules... "
 git submodule sync --recursive >>/dev/null
 git submodule update --init --recursive >>/dev/null
 
+# react-native-builder-bob (used by react-native-webrtc and indirectly by
+# mobile-client) walks up to the docs root and picks up its env-keyed
+# `browserslist` field as babel targets, which newer babel rejects. Hide it
+# for the duration of the build and restore on exit.
+ROOT_PKG=$ROOTDIR/package.json
+cp "$ROOT_PKG" "$ROOT_PKG.bak"
+trap 'mv "$ROOT_PKG.bak" "$ROOT_PKG" 2>/dev/null || true' EXIT INT TERM
+node -e "const fs=require('fs'),p=process.argv[1],j=JSON.parse(fs.readFileSync(p));delete j.browserslist;fs.writeFileSync(p,JSON.stringify(j,null,2)+'\n')" "$ROOT_PKG"
+
 # cd packages/web-client-sdk/
 # yarn && yarn build
 
@@ -27,7 +36,6 @@ yarn && yarn build
 
 cd $ROOTDIR
 cd packages/web-client-sdk/packages/react-native-webrtc/
-npm install
 npm run prepare
 
 cd $ROOTDIR
@@ -37,5 +45,8 @@ yarn && yarn build
 cd $ROOTDIR
 cd packages/js-server-sdk/
 yarn && yarn build
+
+mv "$ROOT_PKG.bak" "$ROOT_PKG"
+trap - EXIT INT TERM
 
 printf "DONE\n"
